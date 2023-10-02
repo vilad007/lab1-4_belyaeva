5703a697f28677659275789a26979be55c82e620
Vladislav <ivanov.hangry@gmail.com>
2023-10-02 20:27:25 +0500

    aaaaaaaaaaaaaaaaaaaaaaa

--- lab1/lab1/settings.py
+++ lab1/lab1/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'laba1'
+    'laba1',
+    'Pages'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'lab1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- lab1/lab1/urls.py
+++ lab1/lab1/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from laba1 import views
+from Pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homePageView)
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
 ]
