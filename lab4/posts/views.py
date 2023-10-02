68053435e51d98f29b5f763b785f5277f471bd91
Vladislav <ivanov.hangry@gmail.com>
2023-10-02 20:56:57 +0500

    laba4

---
+++ lab4/posts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
