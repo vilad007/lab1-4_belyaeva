2890c37af3146a8febe7e6122d44caad8e6365ad
Vladislav <ivanov.hangry@gmail.com>
2023-10-02 22:38:15 +0500

    laba6

--- lab5/lab5/urls.py
+++ lab5/lab5/urls.py
@@ -22,4 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.BlogListView.as_view(), name='home'),
     path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', views.BlogUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', views.BlogDeleteView.as_view(), name='post_delete'),
 ]
