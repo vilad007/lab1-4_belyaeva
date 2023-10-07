9ebf781585e5bcdbddc2f68ca923add5d8dd5717
Vladislav <ivanov.hangry@gmail.com>
2023-10-07 15:46:20 +0500

    laba10

--- lab8/lab8/urls.py
+++ lab8/lab8/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
-    path('', include('pages.urls')),
 ]
